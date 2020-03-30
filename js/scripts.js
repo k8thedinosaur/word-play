e8a7d1424d8587d9afa8d663d488c7854143c0d5
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-30 16:09:40 -0700

    use for loop to pull out words >3

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,23 @@ $(document).ready(function() {
     var sentence = $("#sentence").val();
     console.log(sentence);
 
-  })
+    var words = sentence.split(" ");
+    console.log(words);
+
+    // wordLengths = words.forEach(function(word) {
+    //   return word.length;
+    // })
+    // console.log(wordLengths);
+
+    longWords = words.map(function(word) {
+      if (word.length > 3) {
+        return word;
+      } 
+    });
+    console.log(longWords);
+
+
+
+    event.preventDefault();
+  });
 });
